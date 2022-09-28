f876f3b754868104ce8a3884d062cce59c00c65d
Aleksandr Lukoyanov <liablefish@gmail.com>
2022-09-28 19:02:03 +0300

    question 6 done

--- src/simple_ml_ext.cpp
+++ src/simple_ml_ext.cpp
@@ -5,6 +5,105 @@
 
 namespace py = pybind11;
 
+void print_matrix(const float *X, size_t m, size_t n) {
+    for (size_t i = 0; i < m; ++i)
+    {
+       for (size_t j = 0; j < n; ++j)
+       {
+          std::cout << X[i * n + j] << " ";
+       }
+
+       // Newline for new row
+       std::cout << std::endl;
+    }
+}
+
+void multiply_by_const(
+    float *X,
+    float value,
+    size_t m,
+    size_t n
+) {
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            X[i * n + j] *= value;
+        }
+    }
+}
+
+void subtract(
+    float *X,
+    const float *Z,
+    size_t m,
+    size_t n
+) {
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            X[i * n + j] -= Z[i * n + j];
+        }
+    }
+}
+
+void transpose(
+    const float *X,
+    size_t m,
+    size_t n,
+    float *res
+) {
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            res[j * m + i] = X[i * n + j];
+        }
+    }
+}
+
+void onehot(
+    const unsigned char *y,
+    size_t m,
+    size_t k,
+    float *res
+) {
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < k; ++j) {
+            res[i * k + j] = (y[i] == j) ? 1 : 0;
+        }
+    }
+}
+
+void dot(
+     const float *X,
+     const float *Z,
+     size_t m,
+     size_t n,
+     size_t k,
+     float *res
+) {
+     for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < k; ++j) {
+            for (size_t l = 0; l < n; ++l) {
+                res[i * k + j] += static_cast<double>(X[i * n + l]) * Z[l * k + j];
+            }
+        }
+    }
+}
+
+void softmax(
+    float* Z,
+    size_t m,
+    size_t k
+) {
+	for (size_t i = 0; i < m; ++i) {
+        float exp_sum = 0;
+        for (size_t j = 0; j < k; ++j) {
+             Z[i * k + j] = exp(Z[i * k + j]);
+             exp_sum += Z[i * k + j];
+        }
+        for (size_t j = 0; j < k; ++j) {
+             Z[i * k + j] /= exp_sum;
+        }
+    }
+}
+
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
@@ -33,7 +132,32 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    float lr_step = lr / static_cast<float>(batch);
 
+    float* onehot_y = new float[m * k]();
+    onehot(y, m, k, onehot_y);
+
+    for (size_t i = 0; i < m; i += batch) {
+        float* softmax_res = new float[batch * k]();
+        dot(X + (i * n), theta, batch, n, k, softmax_res);
+        softmax(softmax_res, batch, k);
+        // print_matrix(softmax_res, batch, k);
+        subtract(softmax_res, onehot_y + (i * k), batch, k);
+
+        float *transposed_batch = new float[n * batch]();
+        transpose(X + (i * n), batch, n, transposed_batch);
+
+        float* gradient = new float[n * k]();
+        dot(transposed_batch, softmax_res, n, batch, k, gradient);
+        multiply_by_const(gradient, lr_step, n, k);
+        subtract(theta, gradient, n, k);
+
+        delete[] softmax_res;
+        delete[] transposed_batch;
+        delete[] gradient;
+
+    }
+    delete[] onehot_y;
     /// END YOUR CODE
 }
 
